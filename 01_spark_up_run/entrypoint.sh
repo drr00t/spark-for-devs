cc8e86c37a9c25330a0406ebc8ad2c9fc30b34fa
drr00t <adribeiro@gmail.com>
2024-06-10 22:16:47 -0300

    added history server

--- 01_spark_up_run/entrypoint.sh
+++ 01_spark_up_run/entrypoint.sh
@@ -1,18 +1,24 @@
-#start-spark.sh
 #!/bin/bash
 . "${SPARK_HOME}/bin/load-spark-env.sh"
-# When the spark work_load is master run class org.apache.spark.deploy.master.Master
+
+export SPARK_MASTER_HOST=`hostname`
+
 if [ "$SPARK_WORKLOAD" == "master" ];
 then
 
-export SPARK_MASTER_HOST=`hostname`
+# export SPARK_MASTER_HOST=`hostname`
 
 cd ${SPARK_HOME}/bin && ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
 
 elif [ "$SPARK_WORKLOAD" == "worker" ];
 then
-# When the spark work_load is worker run class org.apache.spark.deploy.master.Worker
+
 cd ${SPARK_HOME}/bin && ./spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG
+
+elif [ "$SPARK_WORKLOAD" == "history" ];
+then
+
+cd ${SPARK_HOME}/bin && ./spark-class org.apache.spark.deploy.history.HistoryServer 
 
 elif [ "$SPARK_WORKLOAD" == "submit" ];
 then
